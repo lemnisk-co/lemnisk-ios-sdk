17f7701eea759c0e0e39e18527e9cba5d5e091ab
Akash Neeli <neeli.akash@lemnisk.co>
2022-03-15 22:48:24 +0530

    Added Clevertap

---
+++ Clevertap/Lemnisk.xcframework/ios-arm64_armv7/Lemnisk.framework/Headers/Lemnisk.h
@@ -0,0 +1,19 @@
+//
+//  Lemnisk.h
+//  Lemnisk
+//
+//  Created by Lemnisk on 23/05/19.
+//  Copyright © 2019 Lemnisk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lemnisk.
+FOUNDATION_EXPORT double LemniskVersionNumber;
+
+//! Project version string for Lemnisk.
+FOUNDATION_EXPORT const unsigned char LemniskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lemnisk/PublicHeader.h>
+
+
